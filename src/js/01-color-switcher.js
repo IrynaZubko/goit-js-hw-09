bded3e8d3a713e2ede7a20688c850ecd7ecc7b28
Iryna Zubko <irinazubko.ntu@gmail.com>
2022-11-08 15:46:10 +0200

    change to the solution with class

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,44 @@
 const refs = {
-    startButton: document.querySelector("button[data-start]"),
-    stopButton: document.querySelector("button[data-stop]"),
-    body: document.querySelector("body"),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+const changeBackgroundColor = () =>
+  (refs.body.style.backgroundColor = colorSwitcher.getRandomHexColor());
+
+class ColorSwitcher {
+  constructor({ onClick }) {
+    this.colorID = null;
+    this.onClick = onClick;
+    refs.stopButton.setAttribute('disabled', true);
+  }
+
+  onStartButtonClick() {
+    this.colorID = setInterval(this.onClick, 1000);
+    refs.startButton.setAttribute('disabled', true);
+    refs.stopButton.removeAttribute('disabled');
+  }
+
+  onStopButtonClick() {
+    clearInterval(this.colorID);
+    refs.startButton.removeAttribute('disabled');
+    refs.stopButton.setAttribute('disabled', true);
+  }
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 }
 
-let colorID = null;
-const changeBackgroundColor = () => refs.body.style.backgroundColor = getRandomHexColor();
+const colorSwitcher = new ColorSwitcher({
+  onClick: changeBackgroundColor,
+});
 
-refs.startButton.addEventListener('click', onStartButtonClick);
-refs.stopButton.addEventListener('click', onStopButtonClick);
+refs.startButton.addEventListener('click', () =>
+  colorSwitcher.onStartButtonClick()
+);
 
-refs.stopButton.setAttribute("disabled", true);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function onStartButtonClick() {
-    colorID = setInterval(changeBackgroundColor, 1000);
-    refs.startButton.setAttribute("disabled", true);
-    refs.stopButton.removeAttribute("disabled");
-}
-
-function onStopButtonClick() {
-    clearInterval(colorID);
-    refs.startButton.removeAttribute("disabled");
-    refs.stopButton.setAttribute("disabled", true);
-}
+refs.stopButton.addEventListener('click', () =>
+  colorSwitcher.onStopButtonClick()
+);
