60617ee90e4c635e46226bf0ac08890d6b336788
Iryna Zubko <irinazubko.ntu@gmail.com>
2022-11-22 16:13:29 +0200

    add the solution

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const firstDelay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const newDelay = firstDelay + step * (i - 1);
+
+    createPromise(i, newDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay,
+        });
+      } else {
+        reject({
+          position,
+          delay,
+        });
+      }
+    }, delay);
+  });
 }
